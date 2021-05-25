27cc9e68918f9bd55370843553f5b18a9c694a73
yusra khan <yusraahmedkhan5@gmail.com>
2021-05-26 00:53:12 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>React App </h2>
+      <h3>Class 2 of Bootcamp 2020</h3>
+    </div>
+  );
+}
+
+export default App;
